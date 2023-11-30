1ef51686e351073d94a2f415bb49574f618332a1
luca-masera <lucamasera1995@gmail.com>
2023-11-30 17:35:36 +0100

    if newPassword

--- partials/main.php
+++ partials/main.php
@@ -13,7 +13,7 @@ if (isset($_GET['passwordLength'])) {
         $randomCharacter = $valori[rand(0, strlen($valori) - 1)];
 
         if (!strpos($newPassword, $randomCharacter)) {
-            $newPassword = $valori;
+            $newPassword .= $valori;
         }
 
     }
@@ -25,25 +25,22 @@ if (isset($_GET['passwordLength'])) {
 
 <main class="container pt-5 ">
     <div>
-        <?php
-        echo $newPassword;
-
-        ?>
+        <?php echo $newPassword ?>
     </div>
     <div>
-        <form action="main.php" methods="GET">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
             <div class="bg-white ps-5 py-5">
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
                         <label for="inputPassword6" class="col-form-label">Lunghezza password:</label>
                     </div>
                     <div class="col-auto">
-                        <input type="password" id="inputPassword6" class="form-control"
+                        <input type="number" id="inputPassword6" class="form-control"
                             aria-describedby="passwordHelpInline" min='5' max='15' name="passwordLength">
 
                     </div>
                 </div>
-                <div class="row g-3 align-items-center ">
+                <!--<div class="row g-3 align-items-center ">
                     <div class="col-auto">
                         <label for="inputPassword6" class="col-form-label">Consenti ripetizioni di uno o più
                             caratteri:</label>
@@ -70,23 +67,24 @@ if (isset($_GET['passwordLength'])) {
                             </label>
                         </div>
                         <div class="form-check ">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
                             <label class="form-check-label" for="flexCheckChecked">
                                 Numeri
                             </label>
                         </div>
                         <div class="form-check ">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
                             <label class="form-check-label" for="flexCheckChecked">
                                 Simboli
                             </label>
                         </div>
                     </div>
 
-                </div>
-                <button type="submit" class="btn btn-primary">Invia</button>
-                <button type="button" class="btn btn-secondary">Annulla</button>
+                </div>-->
+
             </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="button" class="btn btn-secondary">Annulla</button>
 
         </form>
     </div>
